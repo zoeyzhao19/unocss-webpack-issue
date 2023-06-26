be1dcdf6f1edbc3073b1cf5987d2fd74f0e833e3
赵龙生 <zoeyzhao19@gmail.com>
2023-06-26 18:11:35 +0800

    chore: tt

--- App.vue
+++ App.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center">
-    <p class="lg:bg-red-500">The background should be red but it does not</p>
+    <p class="sm:bg-red-500">The background should be red but it does not</p>
   </div>
 </template>
--- uno.config.js
+++ uno.config.js
@@ -3,9 +3,14 @@ import { presetWind } from '@unocss/preset-wind'
 
 export default defineConfig({
   // ...UnoCSS options
+  theme: {
+    breakpoints: {
+      sm: '320px'
+    }
+  },
   presets: [
     presetWind({
-      dark: 'class'
-    })
+      dark: 'class',   
+    }),
   ],
 })
